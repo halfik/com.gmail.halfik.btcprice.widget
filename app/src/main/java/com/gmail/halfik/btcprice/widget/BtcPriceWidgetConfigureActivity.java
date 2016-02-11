a134ebeba71c0ed2b94aa7a8d39fa541309bcaf8
halfik <halfik@gmail.com>
2016-02-11 23:10:59 +0100

    dev

--- app/src/main/java/com/gmail/halfik/btcprice/widget/BtcPriceWidgetConfigureActivity.java
+++ app/src/main/java/com/gmail/halfik/btcprice/widget/BtcPriceWidgetConfigureActivity.java
@@ -6,14 +6,23 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.app.DialogFragment;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 
+import com.github.danielnilsson9.colorpickerview.dialog.ColorPickerDialogFragment;
 import com.gmail.halfik.btcprice.R;
 import com.gmail.halfik.btcprice.service.PollService;
+
+import org.w3c.dom.Text;
 
 import java.util.Collection;
 import java.util.HashMap;
@@ -23,11 +32,17 @@ import java.util.Map;
  * The configuration screen for the {@link BtcPriceWidget BtcPriceWidget} AppWidget.
  */
 public class BtcPriceWidgetConfigureActivity extends Activity
+    implements ColorPickerDialogFragment.ColorPickerDialogListener
 {
     private final static String TAG = "configureActivity";
     int mAppWidgetId = AppWidgetManager.INVALID_APPWIDGET_ID;
     Spinner mSpinnerTime;
     HashMap<Integer, String> mTimeItems = new HashMap<Integer, String>();
+
+    TextView mPriceUpPrimaryColor;
+    TextView mPriceUpSecondaryColor;
+    TextView mPriceDownPrimaryColor;
+    TextView mPriceDownSecondaryColor;
 
     public BtcPriceWidgetConfigureActivity() {
         super();
@@ -44,6 +59,70 @@ public class BtcPriceWidgetConfigureActivity extends Activity
         setContentView(R.layout.btc_price_widget_configure);
         mSpinnerTime = (Spinner) findViewById(R.id.time_spinner);
         findViewById(R.id.add_button).setOnClickListener(mOnClickListener);
+
+        mPriceUpPrimaryColor = (TextView) findViewById(R.id.priceUpPrimaryColor);
+        mPriceUpSecondaryColor = (TextView) findViewById(R.id.priceUpSecondaryColor);
+        mPriceDownPrimaryColor = (TextView) findViewById(R.id.priceDownPrimaryColor);
+
+        mPriceUpPrimaryColor.setBackgroundColor(getResources().getColor(R.color.green1));
+        mPriceUpPrimaryColor.setOnClickListener(new Button.OnClickListener(){
+                                                @Override
+                                                public void onClick(View v) {
+                                                    android.app.FragmentManager manager = getFragmentManager();
+                                                    FragmentTransaction ft = manager.beginTransaction();
+                                                    Fragment prev = manager.findFragmentByTag("priceUpPrimaryColor");
+                                                    if (prev != null) {
+                                                        ft.remove(prev);
+                                                    }
+
+                                                    // Create and show the dialog.
+                                                    DialogFragment newFragment = ColorPickerDialogFragment.newInstance(
+                                                            1,  null, null, getResources().getColor(R.color.green1), true
+                                                    );
+                                                    newFragment.show(ft, "priceUpPrimaryColor");
+                                                }
+                                            }
+        );
+
+        mPriceUpSecondaryColor.setBackgroundColor(getResources().getColor(R.color.green2));
+        mPriceUpSecondaryColor.setOnClickListener(new Button.OnClickListener(){
+                                                    @Override
+                                                    public void onClick(View v) {
+                                                        android.app.FragmentManager manager = getFragmentManager();
+                                                        FragmentTransaction ft = manager.beginTransaction();
+                                                        Fragment prev = manager.findFragmentByTag("priceUpSecondaryColor");
+                                                        if (prev != null) {
+                                                            ft.remove(prev);
+                                                        }
+
+                                                        // Create and show the dialog.
+                                                        DialogFragment newFragment = ColorPickerDialogFragment.newInstance(
+                                                                2,  null, null, getResources().getColor(R.color.green2), true
+                                                        );
+                                                        newFragment.show(ft, "priceUpSecondaryColor");
+                                                    }
+                                                }
+        );
+
+        mPriceDownPrimaryColor.setBackgroundColor(getResources().getColor(R.color.red1));
+        mPriceDownPrimaryColor.setOnClickListener(new Button.OnClickListener(){
+                                                      @Override
+                                                      public void onClick(View v) {
+                                                          android.app.FragmentManager manager = getFragmentManager();
+                                                          FragmentTransaction ft = manager.beginTransaction();
+                                                          Fragment prev = manager.findFragmentByTag("priceUpSecondaryColor");
+                                                          if (prev != null) {
+                                                              ft.remove(prev);
+                                                          }
+
+                                                          // Create and show the dialog.
+                                                          DialogFragment newFragment = ColorPickerDialogFragment.newInstance(
+                                                                  3,  null, null, getResources().getColor(R.color.red1), true
+                                                          );
+                                                          newFragment.show(ft, "priceUpSecondaryColor");
+                                                      }
+                                                  }
+        );
 
         // Find the widget id from the intent.
         Intent intent = getIntent();
@@ -121,5 +200,14 @@ public class BtcPriceWidgetConfigureActivity extends Activity
     };
 
 
+    @Override
+    public void onColorSelected(int dialogId, int color) {
+
+    }
+
+    @Override
+    public void onDialogDismissed(int dialogId) {
+
+    }
 }
 
